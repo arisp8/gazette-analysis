6afbe79407d20bd850e72571ddb7106f44b78a65
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-16 16:48:18 +0300

    Update TransactionHandler to allow select queries

--- db/transaction.py
+++ db/transaction.py
@@ -27,7 +27,11 @@ class TransactionHandler:
 
             count += 1
 
-        query = "INSERT INTO {} ({}) VALUES({})".format(table, columns, values)
+        query = '''
+            INSERT INTO {t} ({c}) 
+            VALUES({v})
+        '''.format(t=table, c=columns, v=values)
+
         cursor.execute(query, params)
         self.__db.commit()
 
@@ -48,6 +52,7 @@ class TransactionHandler:
         formatted_conditions = ""
 
         # The default case with no conditions specified will match everything
+        # @TODO: Benchmark and see if WHERE 1 = 1 costs time, if yes remove the WHERE clause when 0 conditions are given
         if not conditions:
             formatted_conditions = "1 = 1"
         else:
@@ -60,9 +65,9 @@ class TransactionHandler:
 
                 # Default separator of none given is AND
                 if len(conditions[condition_name]) == 3:
-                    separator = conditions[condition_name][2]
+                    separator = " {} ".format(conditions[condition_name][2])
                 else:
-                    separator = "AND"
+                    separator = " AND "
 
                 # Default operator if none given is =
                 operator = "="
@@ -102,9 +107,83 @@ class TransactionHandler:
         # Formatting the conditions for the UPDATE statement
         formatted_conditions = self.format_conditions(conditions)
 
-
-
-        query = "UPDATE {} SET {} WHERE {}".format(table, values, formatted_conditions)
+        query = '''
+            UPDATE {t}
+            SET {v} 
+            WHERE {c}
+        '''.format(t=table, v=values, c=formatted_conditions)
         cursor.execute(query)
 
         self.__db.commit()
+
+    # Formats joins to be used in a SELECT query
+    # @param joins A dictionary in the format table_name : [INNER/LEFT,ON]
+    def format_joins(self, joins = None):
+        formatted_joins = ""
+
+        if joins:
+            for join_table in joins:
+                type = joins[join_table][0].upper()
+                on = joins[join_table][1]
+                formatted_joins += "{type} JOIN ON {on}".format(type=type, on=on)
+
+        return formatted_joins
+
+
+    # Builds a SELECT statement for the SQLite database using the parameters given
+    # @param table The table to select FROM
+    # @param params A list of column names
+    # @param condtions A dictionary containing conditions in the format:
+    #   condition_name : [condition_value, operator, separator]. If separator is not given AND will be used
+    # @param joins A dictionary in the format table_name : [INNER/LEFT,ON]
+    # @TODO: Possibly allow adding LIMIT start, offset
+    def select_query(self, table, columns = None, conditions = None, joins = None):
+
+        # If no columns are given we'll select all columns
+        formatted_columns = "*"
+
+        if columns:
+            formatted_columns = ""
+            separator = ","
+
+            for column_name in columns[:-1]:
+                formatted_columns += column_name + separator
+            else:
+                separator = ""
+                formatted_columns += columns[-1] + separator
+
+        # If no condition is given, we will match everything
+        formatted_conditions = self.format_conditions(conditions)
+        formatted_joins = self.format_joins(joins)
+
+
+        query = '''
+            SELECT {cols} 
+            FROM {t} 
+            {j} 
+            WHERE {cond}
+        '''.format(cols=formatted_columns, t=table,j=formatted_joins,cond=formatted_conditions)
+        return query
+
+    # Selects one element that matches given conditions
+    def select_one(self, table, columns=None, conditions=None, joins=None):
+        cursor = self.__db.cursor()
+        query = self.select_query(table, columns, conditions, joins)
+        cursor.execute(query)
+        return cursor.fetchone()
+
+    # Selects many elements that match given conditions
+    # @param amount The amount of elements to return
+    def select_many(self, table, columns=None, conditions=None, joins=None, limit = 1):
+        cursor = self.__db.cursor()
+        query = self.select_query(table, columns, conditions, joins)
+        print(query)
+        cursor.execute(query)
+        return cursor.fetchmany(limit)
+
+    # Selects all elements that match a query
+    def select_all(self, table, columns=None, conditions=None, joins=None):
+        cursor = self.__db.cursor()
+        query = self.select_query(table, columns, conditions, joins)
+        cursor.execute(query)
+        return cursor.fetchall()
