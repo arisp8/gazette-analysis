ca521fbf7d6dc277c50f43e2547038e19c7b236e
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-18 13:47:33 +0300

    Create handler for ministers.

---
+++ db/handlers/minister.py
@@ -0,0 +1,28 @@
+from mmu.db.transaction import TransactionHandler
+class MinisterHandler(TransactionHandler):
+
+    # Default constructor for Ministers
+    def __init__(self, db_name = 'default'):
+        TransactionHandler.__init__(self, db_name)
+
+    # Loads a minister from the database using his id
+    def load_by_id(self, id):
+        conditions = {'id' : [id]}
+        return self.load(conditions)
+
+    # Loads a minister from the database by matching his name
+    def load_by_name(self, name):
+        conditions = {'name' : ['%name%', 'LIKE']}
+        return self.load(conditions)
+
+    # Selects (up to) one minister who matches the conditions given
+    def load_one(self, conditions = None):
+        return TransactionHandler.select_one(self, table='ministers', conditions=conditions,
+                                         joins={'terms' : ['INNER', 'ministers.id = terms.minister_id'],
+                                                'ministries' : ['INNER', 'ministries.id = terms.ministry_id']})
+
+    # Selects all ministers who match the conditions given
+    def load_all(self, conditions = None):
+        return TransactionHandler.select_all(self, table='ministers', conditions=conditions,
+                                         joins={'terms' : ['INNER', 'ministers.id = terms.minister_id'],
+                                                'ministries' : ['INNER', 'ministries.id = terms.ministry_id']})
