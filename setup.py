70f5de0e57878ed817a6d85e4c9fd7c2eaf208e8
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-19 15:22:30 +0300

    Update setup with new database schema

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 import sqlite3
 import os, errno
+import sys
 
-db = sqlite3.connect('data/default')
+# Sets up required elements for the application
+# @todo: Install required libraries
+def setup():
+    if len(sys.argv) > 1:
+        arg1 = sys.argv[1]
+        if arg1 == '--clean_install':
+            print("Deleting SQLite Database")
+            delete_sqlite_database()
+            print("Setting up local database from scratch")
+            setup_local_db()
+
+def delete_sqlite_database():
+    try:
+        os.remove('data/default')
+        print("SQLite database has been deleted")
+    except FileNotFoundError as e:
+        pass
+
+
 
 # Takes care of setting up the local SQLite database
 def setup_local_db():
+    db = sqlite3.connect('data/default')
+    cursor = db.cursor()
     # Creates the data folder if not exists
     try:
         os.makedirs('data')
@@ -12,86 +33,106 @@ def setup_local_db():
         if e.errno != errno.EEXIST:
             raise
 
-    setup_ministries_table()
-    setup_mps_table()
-    setup_terms_table()
-    setup_issues_table()
-    setup_signatures_table()
+    setup_ministries_table(cursor)
+    setup_persons_table(cursor)
+    setup_cabinets_table(cursor)
+    setup_issues_table(cursor)
+    setup_signatures_table(cursor)
+    setup_positions_table(cursor)
 
 # Sets up default table for ministries
-def setup_ministries_table():
-    cursor = db.cursor()
+def setup_ministries_table(cursor):
+
+    # This table contains all greek ministries. Name changes and restructuring count as different ministries.
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS ministries(
-            id INTEGER PRIMARY KEY,
-            name TEXT
+            id INTEGER PRIMARY KEY, 
+            name TEXT -- The Ministry's name, e.g. "Υπουργείο Οικονομικών" (Ministry of Finance)
         );
     ''')
 
-# Sets up default table for MP's (Members of Parliament)
-def setup_mps_table():
-    cursor = db.cursor()
+# Sets up default table for persons (Most of them Members of Parliament, but not all)
+def setup_persons_table(cursor):
 
-    # Important note: MPs on their own are not attached or affiliated with any ministries by default. This
-    # relationship can only be found from the `terms` table which specifies the time periods where x MP was in
+    # Important note: Persons on their own are not attached or affiliated with any ministries by default. This
+    # relationship can only be found from the `positions` table which specifies the time periods where x person was in
     # y ministry.
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS mps(
+        CREATE TABLE IF NOT EXISTS persons(
             id INTEGER PRIMARY KEY,
-            name TEXT,
-            political_party TEXT,
-            birthdate INTEGER
+            name TEXT NOT NULL, -- A person's full name
+            political_party TEXT, -- The political party of someone, or Independent if there's no affiliation
+            birthdate INTEGER -- UNIX timestamp of a person's date of birth
         );
     ''')
-    db.commit()
 
-# Sets up default table for minister's terms
-def setup_terms_table():
-    cursor = db.cursor()
+# Sets up default table for cabinets
+def setup_cabinets_table(cursor):
+
+    # This table contains the cabinets of greece. An example cabinet is the active one at the time of writing:
+    # Link: https://en.wikipedia.org/wiki/Second_Cabinet_of_Alexis_Tsipras
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS terms (
+        CREATE TABLE IF NOT EXISTS cabinets (
+            id INTEGER PRIMARY KEY, 
+            title TEXT NOT NULL, -- The cabinet's title, e.g. "Κυβέρνηση Αλέξη Τσίπρα Σεπτεμβρίου 2015"
+            description TEXT, -- A general description for the cabinet
+            date_from INTEGER NOT NULL, -- UNIX timestamp containing the day the cabinet was formed
+            date_to INTEGER NOT NULL -- UNIX timestamp containing the day the cabinet disbanded, 0 if still active
+        );
+    ''')
+
+# Sets up default positions table for persons
+def setup_positions_table(cursor):
+
+    # This table specifies in which ministry a person is and during which time
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS positions (
             id INTEGER PRIMARY KEY,
-            mp_id INTEGER,
-            ministry_id INTEGER,
-            date_from INTEGER,
-            date_to INTEGER,
-            FOREIGN KEY(mp_id) REFERENCES mps(id),
+            role TEXT, -- The person's role, e.g. Minister, Deputy etc.
+            date_from INTEGER NOT NULL, -- UNIX timestamp when the person got this position
+            date_to INTEGER NOT NULL, -- UNIX timestamp when the person stopped being in this position
+            person_id INTEGER, -- Shows which person's position we're referencing
+            ministry_id INTEGER, -- Shows in which ministry this person is
+            FOREIGN KEY(person_id) REFERENCES persons(id),
             FOREIGN KEY(ministry_id) REFERENCES ministries(id)
         );
     ''')
 
 # Sets up default table for fek issues
-def setup_issues_table():
-    cursor = db.cursor()
+def setup_issues_table(cursor):
+
+    # Helper table storing information about the fek issues we have saved
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS issues (
             id INTEGER PRIMARY KEY,
-            title TEXT,
-            type TEXT,
-            number INTEGER,
-            file TEXT,
-            date INTEGER
-        );
-            
+            title TEXT, -- The title of the fek issue
+            type TEXT, -- The type of the fek issue, e.g A, B, 	Α.Σ.Ε.Π. etc.
+            number INTEGER, -- The number of the issue
+            file TEXT, -- The issues pdf file location
+            date INTEGER -- UNIX timestamp when this fek issue was published
+        );  
     ''')
 
 # Sets up default table for the signatures found in fek issues
-def setup_signatures_table():
-    cursor = db.cursor()
+def setup_signatures_table(cursor):
+
+    # Holds information about the signatures scraped from the fek issues
+    # Will be useful in the final statistical analysis
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS signatures(
             id INTEGER PRIMARY KEY,
-            mp_id INTEGER,
-            fek_id INTEGER,
-            data TEXT,
-            FOREIGN KEY(mp_id) REFERENCES mps(id),
+            person_id INTEGER, -- The person this signature belongs to
+            fek_id INTEGER, -- The fek issue this signature was scraped from
+            data TEXT, -- Additional data for the signature, possibly extracted from the pdf
+            FOREIGN KEY(person_id) REFERENCES persons(id),
             FOREIGN KEY(fek_id) REFERENCES issues(id)
         );
     ''')
 
-
-setup_local_db()
+setup()
