1ef1e031d32de2d8dc350d6b8ea78502f861739a
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-19 16:48:13 +0300

    Clean up setup.py by creating separate .sql file

---
+++ install/default.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS `signatures` (
+	`id`	INTEGER,
+	`person_id`	INTEGER, -- The person this signature belongs to
+	`fek_id`	INTEGER, -- The fek issue this signature was scraped from
+	`data`	TEXT, -- Additional data for the signature, possibly extracted from the pdf
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`fek_id`) REFERENCES `issues`(`id`),
+	FOREIGN KEY(`person_id`) REFERENCES `persons`(`id`)
+);
+CREATE TABLE IF NOT EXISTS `positions` (
+	`id`	INTEGER,
+	`role`	TEXT, -- The person's role, e.g. Minister, Deputy etc.
+	`date_from`	INTEGER NOT NULL, -- UNIX timestamp when the person got this position
+	`date_to`	INTEGER NOT NULL, -- UNIX timestamp when the person stopped being in this position
+	`person_id`	INTEGER, -- Shows which person's position we're referencing
+	`ministry_id`	INTEGER, -- Shows in which ministry this person is
+	`cabinet_id`	INTEGER, -- References the cabinet in which this position was allocated
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`person_id`) REFERENCES `persons`(`id`),
+	FOREIGN KEY(`ministry_id`) REFERENCES `ministries`(`id`),
+	FOREIGN KEY(`cabinet_id`) REFERENCES `cabinets`(`id`)
+);
+CREATE TABLE IF NOT EXISTS `persons` (
+	`id`	INTEGER,
+	`name`	TEXT NOT NULL, -- The person's full name
+	`political_party`	TEXT, -- The political party of this person, or Independent if there's no affiliation
+	`birthdate`	INTEGER, -- UNIX timestamp of this person's date of birth
+	PRIMARY KEY(`id`)
+);
+CREATE TABLE IF NOT EXISTS `ministries` (
+	`id`	INTEGER,
+	`name`	TEXT, -- The Ministry's name, e.g. "Υπουργείο Οικονομικών" (Ministry of Finance)
+	PRIMARY KEY(`id`)
+);
+CREATE TABLE IF NOT EXISTS `issues` (
+	`id`	INTEGER,
+	`title`	TEXT, -- The title of the fek issue
+	`type`	TEXT, -- The type of the fek issue, e.g A, B, 	Α.Σ.Ε.Π. etc.
+	`number`	INTEGER, -- The number of the issue
+	`file`	TEXT, -- The issues pdf file location
+	`date`	INTEGER, -- UNIX timestamp when this fek issue was published
+	PRIMARY KEY(`id`)
+);
+CREATE TABLE IF NOT EXISTS `cabinets` (
+	`id`	INTEGER,
+	`title`	TEXT NOT NULL, -- The cabinet's title, e.g. "Κυβέρνηση Αλέξη Τσίπρα Σεπτεμβρίου 2015"
+	`description`	TEXT, -- A general description for the cabinet
+	`date_from`	INTEGER NOT NULL, -- UNIX timestamp containing the day the cabinet was formed
+	`date_to`	INTEGER NOT NULL, -- UNIX timestamp containing the day the cabinet disbanded, 0 if still active
+	PRIMARY KEY(`id`)
+);
--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@ def delete_sqlite_database():
         print("File could not be deleted")
 
 
-
-
 # Takes care of setting up the local SQLite database
 def setup_local_db():
     db = sqlite3.connect('data/default')
@@ -36,108 +34,12 @@ def setup_local_db():
         if e.errno != errno.EEXIST:
             raise
 
-    setup_ministries_table(cursor)
-    setup_persons_table(cursor)
-    setup_cabinets_table(cursor)
-    setup_issues_table(cursor)
-    setup_signatures_table(cursor)
-    setup_positions_table(cursor)
+    # Executes the default.sql to create default database schema
+    install_sql = open('install/default.sql', 'r', encoding='utf8').read()
+    cursor.executescript(install_sql)
 
-# Sets up default table for ministries
-def setup_ministries_table(cursor):
-
-    # This table contains all greek ministries. Name changes and restructuring count as different ministries.
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS ministries(
-            id INTEGER PRIMARY KEY, 
-            name TEXT -- The Ministry's name, e.g. "Υπουργείο Οικονομικών" (Ministry of Finance)
-        );
-    ''')
-
-# Sets up default table for persons (Most of them Members of Parliament, but not all)
-def setup_persons_table(cursor):
-
-    # Important note: Persons on their own are not attached or affiliated with any ministries by default. This
-    # relationship can only be found from the `positions` table which specifies the time periods where x person was in
-    # y ministry.
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS persons(
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL, -- A person's full name
-            political_party TEXT, -- The political party of someone, or Independent if there's no affiliation
-            birthdate INTEGER -- UNIX timestamp of a person's date of birth
-        );
-    ''')
-
-# Sets up default table for cabinets
-def setup_cabinets_table(cursor):
-
-    # This table contains the cabinets of greece. An example cabinet is the active one at the time of writing:
-    # Link: https://en.wikipedia.org/wiki/Second_Cabinet_of_Alexis_Tsipras
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS cabinets (
-            id INTEGER PRIMARY KEY, 
-            title TEXT NOT NULL, -- The cabinet's title, e.g. "Κυβέρνηση Αλέξη Τσίπρα Σεπτεμβρίου 2015"
-            description TEXT, -- A general description for the cabinet
-            date_from INTEGER NOT NULL, -- UNIX timestamp containing the day the cabinet was formed
-            date_to INTEGER NOT NULL -- UNIX timestamp containing the day the cabinet disbanded, 0 if still active
-        );
-    ''')
-
-# Sets up default positions table for persons
-def setup_positions_table(cursor):
-
-    # This table specifies in which ministry a person is and during which time
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS positions (
-            id INTEGER PRIMARY KEY,
-            role TEXT, -- The person's role, e.g. Minister, Deputy etc.
-            date_from INTEGER NOT NULL, -- UNIX timestamp when the person got this position
-            date_to INTEGER NOT NULL, -- UNIX timestamp when the person stopped being in this position
-            person_id INTEGER, -- Shows which person's position we're referencing
-            ministry_id INTEGER, -- Shows in which ministry this person is
-            cabinet_id INTEGER, -- References the cabinet in which this position was allocated
-            FOREIGN KEY(person_id) REFERENCES persons(id),
-            FOREIGN KEY(ministry_id) REFERENCES ministries(id),
-            FOREIGN KEY(cabinet_id) REFERENCES cabinets(id)
-        );
-    ''')
-
-# Sets up default table for fek issues
-def setup_issues_table(cursor):
-
-    # Helper table storing information about the fek issues we have saved
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS issues (
-            id INTEGER PRIMARY KEY,
-            title TEXT, -- The title of the fek issue
-            type TEXT, -- The type of the fek issue, e.g A, B, 	Α.Σ.Ε.Π. etc.
-            number INTEGER, -- The number of the issue
-            file TEXT, -- The issues pdf file location
-            date INTEGER -- UNIX timestamp when this fek issue was published
-        );  
-    ''')
-
-# Sets up default table for the signatures found in fek issues
-def setup_signatures_table(cursor):
-
-    # Holds information about the signatures scraped from the fek issues
-    # Will be useful in the final statistical analysis
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS signatures(
-            id INTEGER PRIMARY KEY,
-            person_id INTEGER, -- The person this signature belongs to
-            fek_id INTEGER, -- The fek issue this signature was scraped from
-            data TEXT, -- Additional data for the signature, possibly extracted from the pdf
-            FOREIGN KEY(person_id) REFERENCES persons(id),
-            FOREIGN KEY(fek_id) REFERENCES issues(id)
-        );
-    ''')
+    db.commit()
+    cursor.close()
+    db.close()
 
 setup()
