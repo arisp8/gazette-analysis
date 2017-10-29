02769aeb349db023a9bacae564528cf7f697af12
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-29 18:21:15 +0200

    Update ministry handler to properly load and create records

--- db/handlers/ministry.py
+++ db/handlers/ministry.py
@@ -7,11 +7,17 @@ class MinistryHandler(TransactionHandler):
         TransactionHandler.__init__(self, db_name)
 
     # Creates a new ministry
-    def create(self, name):
-        params = {'name:' : name}
+    def create(self, name, description, established = 0, disbanded = 0):
+        params = {'name' : name, 'description' : description, 'established' : established, 'disbanded' : disbanded}
+        print(params)
         TransactionHandler.insert(self,table='ministries',params=params)
 
     # Updates a ministry based on the id given
     def update(self, id, params):
         conditions = {'id' : [id]}
         TransactionHandler.update(self, table='ministries', params=params, conditions=conditions)
+
+    # Loads a ministry by name
+    def load_by_name(self, name):
+        conditions = {'name' : [name]}
+        return TransactionHandler.select_one(self, 'ministries', conditions=conditions)
