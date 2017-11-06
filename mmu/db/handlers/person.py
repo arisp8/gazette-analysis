a59ec74825e796d8205f2f2a511b73ced940a1b9
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-06 04:24:05 +0200

    Update persons handler with more methods

--- mmu/db/handlers/person.py
+++ mmu/db/handlers/person.py
@@ -14,27 +14,33 @@ class PersonHandler(TransactionHandler):
 
     # Loads a person from the database by matching his name
     def load_by_name(self, name):
-        conditions = {'name' : ['%name%', 'LIKE']}
+        conditions = {'name' : ['%' + name + '%', 'LIKE']}
         return self.load_one(conditions)
 
     # Selects (up to) one person who matches the conditions given
     def load_one(self, conditions = None):
-        return TransactionHandler.select_one(self, table='persons', conditions=conditions,
-                                         joins={'positions' : ['INNER', 'persons.id = positions.person_id'],
-                                                'ministries' : ['INNER', 'ministries.id = positions.ministry_id']})
+        return TransactionHandler.select_one(self, table='persons', conditions=conditions)
 
     # Selects all persons who match the conditions given
     def load_all(self, conditions = None):
-        return TransactionHandler.select_all(self, table='persons', conditions=conditions,
-                                         joins={'terms' : ['INNER', 'persons.id = positions.person_id'],
-                                                'ministries' : ['INNER', 'ministries.id = positions.ministry_id']})
+        return TransactionHandler.select_all(self, table='persons', conditions=conditions)
 
     # Creates a new person
     def create(self, name, political_party, birthdate):
-        TransactionHandler.insert(self, 'persons', {'name' : name, 'political_party' : political_party,
-                                                      'birthdate' : birthdate})
+        values = {'name' : name, 'political_party' : political_party, 'birthdate' : birthdate}
+        TransactionHandler.insert(self, table='persons', params=values)
 
     # Updates a person's information
     def update(self, id, params):
         conditions = {'id' : [id]}
         TransactionHandler.update(self, table='persons', params=params, conditions=conditions)
+
+    # Create a new position
+    def save_position(self, role, date_from, date_to, person_id, ministry_id, cabinet_id):
+        values = {'role' : role, 'date_from' : date_from, 'date_to' : date_to, 'person_id' : person_id,
+                  'ministry_id' : ministry_id, 'cabinet_id' : cabinet_id}
+        TransactionHandler.insert(self, table='positions', params=values)
+
+    # Loads a position given some conditions
+    def load_position(self, conditions):
+        return TransactionHandler.select_one(self, 'positions', conditions=conditions)
