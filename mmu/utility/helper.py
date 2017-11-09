87cfa8e399207c6309c8fdf49b18e37094b57b59
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-09 23:56:22 +0200

    Fix bug when normalizing names

--- mmu/utility/helper.py
+++ mmu/utility/helper.py
@@ -2,15 +2,13 @@
 class Helper:
 
     # Turns a greek name to all caps and without accents
+    # @todo: Use regex or other way to speed up
     def normalize_greek_name(self, name):
         # α, β, γ, δ, ε, ζ, η, θ, ι, κ, λ, μ, ν, ξ, ο, π, ρ, σ, τ, υ, φ, χ, ψ, ω
         name = name.upper()
-        replace_accents = {'Ά': 'Α', 'Έ': 'Ε', 'Ή': 'Η', 'Ί': 'Ι', 'Ϊ': 'Ι', 'Ό' : 'Ο', 'Ύ': 'Υ', 'Ϋ' : 'Υ', 'Ώ' : 'Ω'}
+        replace_accents = {'Ά': 'Α', 'Έ': 'Ε', 'Ή': 'Η', 'Ί': 'Ι', 'Ϊ': 'Ι', 'Ό': 'Ο', 'Ύ': 'Υ', 'Ϋ': 'Υ', 'Ώ': 'Ω'}
 
         for accent in replace_accents:
-            print(accent)
-            print(replace_accents[accent])
-            name.replace(accent, replace_accents[accent])
+            name =  name.replace(accent, replace_accents[accent])
 
         return name
-
