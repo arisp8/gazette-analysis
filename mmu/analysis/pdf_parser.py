27a261db2cb6cdc9d5bb69b4b6c56f62a8ebf6e8
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-09 18:30:37 +0200

    Create basic PDF Parser to read text

---
+++ mmu/analysis/pdf_parser.py
@@ -0,0 +1,43 @@
+import os
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+class PdfParser:
+
+    def __init__(self):
+        self.__project_path = os.getcwd()
+
+    def get_raw_data(self, file_name):
+        print(self.__project_path + file_name)
+        text = self.convert_pdf_to_txt(self.__project_path + file_name)
+        print(text)
+
+    def convert_pdf_to_txt(self, path):
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = open(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                      password=password,
+                                      caching=caching,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+
+        text = retstr.getvalue()
+
+        fp.close()
+        device.close()
+        retstr.close()
+        return text
