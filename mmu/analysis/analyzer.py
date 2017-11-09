41ff9a8db57e1f40012f45a03adb4a4a65e0de3c
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-09 18:31:19 +0200

    Extract pdf data from all issues

--- mmu/analysis/analyzer.py
+++ mmu/analysis/analyzer.py
@@ -1,10 +1,19 @@
 from mmu.db.handlers.issue import IssueHandler
+from mmu.analysis.pdf_parser import PdfParser
 
 class Analyzer:
 
     def __init__(self):
         self.__issue_handler = IssueHandler()
+        self.__pdf_analyzer = PdfParser()
 
     def start_analysis(self):
         issues = self.__issue_handler.load_all()
-        print(issues)
+
+        for issue in issues:
+            issue_id = issue['id']
+            issue_file = issue['file']
+
+            raw_pdf_data = self.__pdf_analyzer.get_raw_data(issue_file)
+
+
