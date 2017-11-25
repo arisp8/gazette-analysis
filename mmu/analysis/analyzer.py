eb75244d9bce9bee3694ba957e15d957953dae87
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-25 20:25:33 +0200

    Locate signatures in text

--- mmu/analysis/analyzer.py
+++ mmu/analysis/analyzer.py
@@ -7,8 +7,35 @@ class Analyzer:
         self.__issue_handler = IssueHandler()
         self.__pdf_analyzer = CustomPDFParser()
 
+    # Finds all occurences of a substring in a string
+    # @return The indexes of all matched substrings.
+    def find_all(self, string, key):
+        matches = []
+        start = 0
+        searching = True
+        while searching:
+            index = string.find(key, start)
+
+            if index == -1:
+                searching = False
+            else:
+                matches.append(index)
+                start = index + 1
+
+        return matches
+
+    # Analyzes the text from the pdf files to extract all signatures
+    def extract_signatures_from_text(self, text):
+        # print(text)
+        signatures = self.find_all(text, "Οι Υπουργοί")
+        for index in signatures:
+            substring = text[index:]
+            print(substring)
+
+
     def start_analysis(self):
-        issues = self.__issue_handler.load_all()
+        # Loads all issues not yet analyzed
+        issues = self.__issue_handler.load_all({'analyzed' : [0]})
 
         for issue in issues:
             issue_id = issue['id']
@@ -16,6 +43,8 @@ class Analyzer:
 
             pdf_text = self.__pdf_analyzer.get_pdf_text(issue_file)
             pdf_images = self.__pdf_analyzer.get_pdf_images(issue_file, issue_id)
-            
+
+            text_signatures = self.extract_signatures_from_text(pdf_text)
+
 
 
