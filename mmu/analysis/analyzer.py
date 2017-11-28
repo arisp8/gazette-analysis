fe72230102fc7932ad4660f59cdbb076b124f141
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-28 15:27:22 +0200

    Extract some names and positions from pdf text

--- mmu/analysis/analyzer.py
+++ mmu/analysis/analyzer.py
@@ -1,5 +1,6 @@
 from mmu.db.handlers.issue import IssueHandler
 from mmu.analysis.pdf_parser import CustomPDFParser
+import re
 
 class Analyzer:
 
@@ -7,9 +8,20 @@ class Analyzer:
         self.__issue_handler = IssueHandler()
         self.__pdf_analyzer = CustomPDFParser()
 
+        # Compile regular expressions that will be used a lot
+        self.__date_pattern = re.compile("[α-ωΑ-Ωά-ώϊ-ϋΐ-ΰ]+,\s+?[0-9]{1,2}\s+?[α-ζΑ-Ζά-ώϊ-ϋΐ-ΰ]+\s+?[0-9]{4,4}")
+        self.__break_pattern = re.compile("[α-ωΑ-Ωά-ώϊ-ϋΐ-ΰ\s+?]|(\n\s*\n)")
+
+    # Checks whether or not a string indicates that parsing should stop.
+    def is_break_point(self, word):
+        if len(word) == 0:
+            return False
+        else:
+            return not self.__break_pattern.match(word)
+
     # Finds all occurences of a substring in a string
     # @return The indexes of all matched substrings.
-    def find_all(self, string, key):
+    def find_all(self, key, string):
         matches = []
         start = 0
         searching = True
@@ -26,11 +38,57 @@ class Analyzer:
 
     # Analyzes the text from the pdf files to extract all signatures
     def extract_signatures_from_text(self, text):
-        # print(text)
-        signatures = self.find_all(text, "Οι Υπουργοί")
-        for index in signatures:
-            substring = text[index:]
-            print(substring)
+        start_keys = ["Οι Υπουργοί", "Οι Αναπληρωτές Υπουργοί"]
+        end_key = "Θεωρήθηκε και τέθηκε η Μεγάλη Σφραγίδα του Κράτους."
+        starting_indexes = []
+
+        for key in start_keys:
+            starting_indexes += self.find_all(key, text)
+
+        if len(starting_indexes) > 1:
+            print(starting_indexes)
+
+        if not starting_indexes:
+            starting_indexes = [m.start() for m in self.__date_pattern.finditer(text)]
+            print('New starts: ', starting_indexes)
+
+
+        # Ending indexes are useful when available, but availability is not guaranteed
+        ending_indexes = self.find_all(end_key, text)
+
+        for key, index in enumerate(starting_indexes):
+            persons = []
+
+            # Indicates whether or not the substring contains non-required information after the signatures
+            # Is True when a valid ending index has been found
+            end = (key < len(ending_indexes) and ending_indexes[key] > index)
+
+            if end:
+                to = ending_indexes[key]
+                substring = text[index:to]
+            else:
+                substring = text[index:]
+
+            words = substring.split("\n")
+
+            # Iterates through words after the starting key
+            role = ""
+            for word in words[1:]:
+
+                # If a valid ending point has not been found, then the first non-word will surely indicate the end of
+                # the signatures.
+                if not end and self.is_break_point(word):
+                    break
+
+                if len(word) > 3 and word.upper() == word:
+                    persons.append({"role" : role, "name" : word})
+                    # Reset role for the next one
+                    role = ""
+                else:
+                    role += word
+
+            print(persons)
+
 
 
     def start_analysis(self):
@@ -40,10 +98,12 @@ class Analyzer:
         for issue in issues:
             issue_id = issue['id']
             issue_file = issue['file']
+            issue_number = issue['number']
 
             pdf_text = self.__pdf_analyzer.get_pdf_text(issue_file)
             pdf_images = self.__pdf_analyzer.get_pdf_images(issue_file, issue_id)
 
+            print("Extracting signatures from issue {}".format(issue_number))
             text_signatures = self.extract_signatures_from_text(pdf_text)
 
 
