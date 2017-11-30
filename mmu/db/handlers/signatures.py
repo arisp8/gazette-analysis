c61b822406a403b38ea01842dad5bbfcb60c00b1
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-30 13:18:11 +0200

    Allow loading signatures by person name

--- mmu/db/handlers/signatures.py
+++ mmu/db/handlers/signatures.py
@@ -12,10 +12,21 @@ class SignatureHandler(TransactionHandler):
         conditions = {'id' : [id]}
         return self.load_one(conditions)
 
+    # Loads all signatures by person_name when given conditions are matched
+    def load_all_by_person_name(self, person_name, conditions = None):
+        joins = {'persons' : ['INNER', 'signatures.person_id = persons.id']}
+
+        if not conditions:
+            conditions = {}
+
+        conditions['persons.name'] = [person_name]
+        print(conditions)
+        return TransactionHandler.select_all(self, table='signatures', conditions=conditions, joins=joins)
+
     # Selects (up to) one signature that matches the conditions given
-    def load_one(self, conditions = None, table = 'signatures'):
-        return TransactionHandler.select_one(self, table=table, conditions=conditions)
+    def load_one(self, conditions = None):
+        return TransactionHandler.select_one(self, table='signatures', conditions=conditions)
 
     # Selects all signatures that match the conditions given
-    def load_all(self, conditions = None, table = 'signatures'):
-        return TransactionHandler.select_all(self, table=table, conditions=conditions)
+    def load_all(self, conditions = None):
+        return TransactionHandler.select_all(self, table='signatures', conditions=conditions)
