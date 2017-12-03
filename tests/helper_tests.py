d7bd270e75eb43827a8c79ee7ab41987fa0c6280
Aris Pattakos <aris.pattakos@gmail.com>
2017-12-04 01:52:38 +0200

    Add test for date conversion

--- tests/helper_tests.py
+++ tests/helper_tests.py
@@ -1,11 +1,21 @@
 import unittest
 from mmu.utility.helper import Helper
+import datetime
 
 class HelperTest(unittest.TestCase):
 
     def test_normalize_greek_name(self):
         self.assertEqual(Helper.normalize_greek_name("Παναγιώτης Καμμένος"), "ΠΑΝΑΓΙΩΤΗΣ ΚΑΜΜΕΝΟΣ")
         self.assertEqual(Helper.normalize_greek_name("Αγλαΐα Τεστ"), "ΑΓΛΑΙΑ ΤΕΣΤ")
+
+    def test_date_to_unix_timestamp(self):
+        self.assertEqual(Helper.date_to_unix_timestamp("22 Ιανουαρίου 2016"), datetime.datetime(2016, 1, 22))
+        self.assertEqual(Helper.date_to_unix_timestamp("08 Αυγούστου 1922"), datetime.datetime(1922, 8, 8))
+        self.assertEqual(Helper.date_to_unix_timestamp("1952"), datetime.datetime(1952, 1, 1))
+
+        # That's my birthday
+        self.assertEqual(Helper.date_to_unix_timestamp("21-08-1998"), datetime.datetime(1998, 8, 21))
+        self.assertEqual(Helper.date_to_unix_timestamp("21/08/1998"), datetime.datetime(1998, 8, 21))
 
 
 if __name__ == '__main__':
