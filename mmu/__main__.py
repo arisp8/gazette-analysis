dbea7264b0c51c584d368d50436f5796cc52f556
Aris Pattakos <aris.pattakos@gmail.com>
2017-12-09 01:50:04 +0200

    Add mmu's parent directory to the python path

--- mmu/__main__.py
+++ mmu/__main__.py
@@ -1,10 +1,18 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(sys.argv[0])[0], '..')))
 from mmu.automations.loader import Loader
 from mmu.automations.researcher import Researcher
+from mmu.analysis.analyzer import Analyzer
+
 
 source = "http://www.et.gr/idocs-nph/search/fekForm.html"
 
 loader = Loader(source)
 loader.scrape_pdfs()
 
-researcher = Researcher(2012, 2017)
+researcher = Researcher()
 researcher.research()
+
+analyzer = Analyzer()
+analyzer.start_analysis()
