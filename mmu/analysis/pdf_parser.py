7592594b2c054050d8325c886285f6aa90f09af7
Aris Pattakos <aris.pattakos@gmail.com>
2017-12-14 21:04:41 +0200

    Extract signatures more efficiently by analyzing the pdf structure

--- mmu/analysis/pdf_parser.py
+++ mmu/analysis/pdf_parser.py
@@ -6,8 +6,11 @@ from subprocess import call
 from PIL import Image
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+from pdfminer.layout import LTTextBox
+from pdfminer.layout import LTTextLine
 from pdfminer.pdfpage import PDFPage
 from mmu.utility.helper import Helper
 
@@ -52,6 +55,81 @@ class CustomPDFParser:
 
 
         return images
+
+    # Analyzes the structure of the pdf file to correctly extract the signatures from the document.
+    def get_signatures_from_pdf(self, path):
+        codec = 'utf-8'
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr=rsrcmgr,laparams=laparams)
+        fp = open(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+        pages = PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                  check_extractable=True)
+
+        # Analyze first page to get a feel of what's going on
+        try:
+            first_page = next(pages)
+            interpreter.process_page(first_page)
+        except StopIteration:
+            print("The pdf document may be damaged")
+            return
+
+        first_page_layout = device.get_result()
+        useful_data = self.get_document_info(first_page_layout)
+
+    # Analyzes the first page of a pdf document to extract useful info about the laws being analyzed.
+    def get_document_info(self, page):
+        # This list will contain information regarding all regulations inside the parsed document. Regulations may be:
+        # -- Laws
+        # -- Presidential Decrees
+        # -- Ministerial Decisions
+        regulations = []
+
+        # These keywords indicate that the document contains more than one regulation and that they are described in the
+        # table of contents.
+        multiple = ['ΚΑΝΟΝΙΣΜΟΙ','ΑΠΟΦΑΣΕΙΣ', 'ΠΕΡΙΕΧΟΜΕΝΑ','ΠΡΑΞΕΙΣ ΥΠΟΥΡΓΙΚΟΥ ΣΥΜΒΟΥΛΙΟΥ', 'ΠΡΟΕΔΡΙΚΑ ΔΙΑΤΑΓΜΑΤΑ',
+                    'ΑΝΑΚΟΙΝΩΣΕΙΣ', 'AΠΟΦΑΣΕΙΣ']
+
+        # These keywords indicate that the document contains a single regulation of the same type as the keyword
+        single =  ['ΠΡΟΕΔΡΙΚΟ ΔΙΑΤΑΓΜΑ ΥΠ’ ΑΡΙΘΜ.', "ΝΟΜΟΣ ΥΠ’ ΑΡΙΘ.", "ΝΟΜΟΣ ΥΠ’ ΑΡΙθ.", "NOMOΣ ΥΠ’ ΑΡΙΘ.",
+                   'ΚΑΝΟΝΙΣΜΟΣ ΥΠ’ ΑΡΙΘΜ.', 'ΝΟΜΟΣ ΥΠ’ ΑΡΙΘΜ.', 'ΠΡΟΕΔΡΙΚΟ ΔΙΑΤΑΓΜΑ ΥΠ΄ ΑΡΙΘΜ.', 'NOMOΣ ΥΠ’ ΑΡΙΘΜ.',
+                   'NOMOΣ ΥΠ’ ΑΡΙΘM.', 'NOMOΣ ΥΠ΄ΑΡΙΘΜ.', 'ΝΟΜΟΣ ΥΠ’ ΑΡΙΘM.', "ΠΡΟΕΔΡΙΚΟ ΔΙΑΤΑΓΜΑ ΥΠ' ΑΡΙΘΜ."]
+
+        # These starting titles let us know that the document doesn't contain relevant data.
+        ignore = ['ΒΟΥΛΗ ΤΩΝ ΕΛΛΗΝΩΝ', 'ΔΙΟΡΘΩΣΕΙΣ ΣΦΑΛΜΑΤΩΝ']
+
+        multiple_indicator = ""
+        text_items = []
+        print(page)
+        # Get the first 10 textual elements inside the pdf.
+        for index, layout_object in enumerate(page):
+
+            if isinstance(layout_object, LTTextBox) or isinstance(layout_object, LTTextLine):
+                text = layout_object.get_text()
+                text_items.append(text)
+
+            if index == 9:
+                break
+
+        found = False
+        for possible_title in text_items[4:10]:
+            for item in multiple + single + ignore:
+                # print(item.lower(), '---', possible_title)
+                if item in possible_title:
+                    found = True
+                    break
+
+        if not found:
+            print(text_items)
+            # for possible_title in text_items[4:8]:
+            #     for item in multiple + single + ignore:
+            #         print(item, '---', possible_title)
 
     def convert_pdf_to_txt(self, path):
         start = timer()
