06e6f46de834becce8817edd4a34ffd1faf6ecba
Aris Pattakos <aris.pattakos@gmail.com>
2018-01-08 02:29:14 +0200

    Check only extracted names in unit test

--- tests/pdf_parser_tests.py
+++ tests/pdf_parser_tests.py
@@ -17,34 +17,23 @@ class PdfParserTest(unittest.TestCase):
         self.assertEqual(regulation['type'], 'ΠΡΟΕΔΡΙΚΟ ΔΙΑΤΑΓΜΑ ΥΠ’ ΑΡΙΘΜ.')
 
     def test_one_regulation_per_document_2(self):
-        correct_signatures = [{"role": "Ο ΠΡΟΕΔΡΟΣ ΤΗΣ ΔΗΜΟΚΡΑΤΙΑΣ", "name": "ΠΡΟΚΟΠΙΟΣ Β ΠΑΥΛΟΠΟΥΛΟΣ"},
-                              {"role": "ΑΝΑΠΛΗΡΩΤΗΣ ΥΠΟΥΡΓΟΣ ΕΣΩΤΕΡΙΚΩΝ ΚΑΙ ΔΙΟΙΚΗΤΙΚΗΣ ΑΝΑΣΥΓΚΡΟΤΗΣΗΣ",
-                               "name": "ΝΙΚΟΛΑΟΣ ΤΟΣΚΑΣ"},
-                              {"role": "ΟΙΚΟΝΟΜΙΑΣ ΑΝΑΠΤΥΞΗΣ ΚΑΙ ΤΟΥΡΙΣΜΟΥ", "name": "ΓΕΩΡΓΙΟΣ ΣΤΑΘΑΚΗΣ"},
-                              {"role": "ΑΝΑΠΛΗΡΩΤΡΙΑ ΥΠΟΥΡΓΟΣ ΟΙΚΟΝΟΜΙΑΣ ΑΝΑΠΤΥΞΗΣ ΚΑΙ ΤΟΥΡΙΣΜΟΥ",
-                               "name": "ΕΛΕΝΑ ΚΟΥΝΤΟΥΡΑ"},
-                              {"role": "ΕΞΩΤΕΡΙΚΩΝ", "name": "ΝΙΚΟΛΑΟΣ ΚΟΤΖΙΑΣ"},
-                              {"role": "ΔΙΚΑΙΟΣΥΝΗΣ ΔΙΑΦΑΝΕΙΑΣ ΚΑΙ ΑΝΘΡΩΠΙΝΩΝ ΔΙΚΑΙΩΜΑΤΩΝ",
-                               "name": "ΝΙΚΟΛΑΟΣ ΠΑΡΑΣΚΕΥΟΠΟΥΛΟΣ"},
-                              {"role": "ΕΡΓΑΣΙΑΣ ΚΟΙΝΩΝΙΚΗΣ ΑΣΦΑΛΙΣΗΣ ΚΑΙ ΚΟΙΝΩΝΙΚΗΣ ΑΛΛΗΛΕΓΓΥΗΣ",
-                               "name": "ΓΕΩΡΓΙΟΣ ΚΑΤΡΟΥΓΚΑΛΟΣ"},
-                              {"role": "ΟΙΚΟΝΟΜΙΚΩΝ", "name": "ΕΥΚΛΕΙΔΗΣ ΤΣΑΚΑΛΩΤΟΣ"},
-                              {"role": "ΝΑΥΤΙΛΙΑΣ ΚΑΙ ΝΗΣΙΩΤΙΚΗΣ ΠΟΛΙΤΙΚΗΣ", "name": "ΘΕΟΔΩΡΟΣ ΔΡΙΤΣΑΣ"}]
+
+        correct_names = ['ΠΡΟΚΟΠΙΟΣ Β ΠΑΥΛΟΠΟΥΛΟΣ', 'ΝΙΚΟΛΑΟΣ ΤΟΣΚΑΣ', 'ΓΕΩΡΓΙΟΣ ΣΤΑΘΑΚΗΣ', 'ΕΛΕΝΑ ΚΟΥΝΤΟΥΡΑ',
+                         'ΝΙΚΟΛΑΟΣ ΚΟΤΖΙΑΣ', 'ΝΙΚΟΛΑΟΣ ΠΑΡΑΣΚΕΥΟΠΟΥΛΟΣ', 'ΓΕΩΡΓΙΟΣ ΚΑΤΡΟΥΓΚΑΛΟΣ',
+                         'ΕΥΚΛΕΙΔΗΣ ΤΣΑΚΑΛΩΤΟΣ', 'ΘΕΟΔΩΡΟΣ ΔΡΙΤΣΑΣ']
+
+
 
         file_path = os.path.join(os.path.join(os.getcwd(), 'test_pdfs/'), 'ΦΕΚ A 12 - 01.02.2016.pdf')
         regulation = self.parser.get_signatures_from_pdf(file_path, str(2016))[0]
+        names = []
 
-        self.assertEqual(correct_signatures, regulation['signatures'])
+        for signature in regulation['signatures']:
+            names.append(signature['name'])
+
+        self.assertListEqual(correct_names, names)
         self.assertEqual(regulation['number'], '4363')
         self.assertEqual(regulation['type'], 'NOMOΣ ΥΠ’ ΑΡΙΘ.')
-
-
-    # def extracts_signatures_from_document_with_multiple_regulations(self):
-    #     signatures = [{"role": "Ο ΠΡΟΕΔΡΟΣ ΤΗΣ ΔΗΜΟΚΡΑΤΙΑΣ", "name": "Ο ΠΡΟΕΔΡΟΣ ΤΗΣ ΔΗΜΟΚΡΑΤΙΑΣ"},
-    #                   {"role": "Ο ΥΠΟΥΡΓΟΣ ΝΑΥΤΙΛΙΑΣ ΚΑΙ ΝΗΣΙΩΤΙΚΗΣ ΠΟΛΙΤΙΚΗΣ", "name": "ΘΕΟΔΩΡΟΣ ΔΡΙΤΣΑΣ"}]
-    #     print(os.getcwd())
-    #     file_path = os.path.join(os.path.join(__file__, 'test_cases/'), 'ΦΕΚ A 1 - 12.01.2016.pdf')
-    #     self.assertEqual(self.parser.get_signatures_from_pdf(file_path))
 
 
 if __name__ == '__main__':
